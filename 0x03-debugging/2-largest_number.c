c21b7489efcfc5cc00dd97d86e4836241d28455a
Chidoski111 <valentinechidera28@gmail.com>
2023-07-21 18:22:42 -0700

    largest number fn updated

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,24 +5,24 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
+ * Return: largest number
  */
-
 int largest_number(int a, int b, int c)
 {
-	int larger;
+    int largest;
 
-	if (a >=b && a >= c)
-	{
-		largest = a;
-	}
-	else if (b >= a && b>= c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
+    if (a >= b && a >= c)
+    {
+        largest = a;
+    }
+    else if (b >= a && b >= c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
 
-	return (largest);
+    return (largest);
 }
